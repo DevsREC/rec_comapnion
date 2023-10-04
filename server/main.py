476394b683b659844f660648427d24319672f713
raxen001 <raxen001@tutamail.com>
2023-10-04 20:53:37 +0530

    added get requests

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,9 +10,121 @@ app = Flask(__name__)
 # read from excel file
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! </p>"
+def create_tt():
+    # TODO add parameters
+    # add a way to check if the response cnotains all the expected parameters
+
+    # debug #
+    days = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', 'Sunday']
+    time = "20231004"
+    class_name = "5-it-b"  # sem dep sec
+    classes = {
+        days[0]: [
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["break", "break"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["Lunch", "Lunch"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+        ],
+        days[1]: [
+
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["break", "break"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["Lunch", "Lunch"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+        ],
+        days[2]: [
+
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["break", "break"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["Lunch", "Lunch"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+        ],
+        days[3]: [
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["break", "break"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["Lunch", "Lunch"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+        ],
+        days[4]: [
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["break", "break"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["Lunch", "Lunch"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+        ],
+        days[5]: [
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["break", "break"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+            ["Lunch", "Lunch"],
+            ["ITA1581", "ComputerNetworks"],
+            ["ITA1581", "ComputerNetworks"],
+        ],
+        days[6]: [
+            ["Why", "did"],
+            ["you", "join"],
+            ["break", "break"],
+            ["this", "clg"],
+            ["da", "sunday"],
+            ["class", "ku"],
+            ["Lunch", "Lunch"],
+            ["coming", ":("],
+            ["L", "L"],
+        ]
+    }
+    ######
+
+    data = {
+        "last_updated": time,
+        "class_name": class_name,
+        "classes": classes
+    }
+
+    data = jsonify(data)
+    return data
+
+
+@app.route("/<name>", methods=['GET', 'POST'])
+def hello_world(name):
+    # get api method
+    if request.method == 'GET':
+
+        data = create_tt()
+        if data:
+            return data
+        else:
+            return "Error"
+
+    if request.method == 'POST':
+        return "POST REQ recieved"
 
 
 if __name__ == "__main__":
