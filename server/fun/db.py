34d07417640cf53e1ce36b6edcd8ace8972328a0
Alien501 <cvignesh404@gmail.com>
2023-10-19 22:54:50 +0530

    added functionality where user can upload timetable via form to MySql server

---
+++ server/fun/db.py
@@ -0,0 +1,119 @@
+import mysql.connector as db
+import datetime
+
+
+import mysql.connector as sql
+import datetime
+import json
+
+def upload_timetable(data):
+    # try:
+    class_name = data['class_name']
+    query1 = f"create table {class_name}(day_name varchar(20), period_1 varchar(100), period_2 varchar(100), period_3 varchar(100), period_4 varchar(100), period_5 varchar(100), period_6 varchar(100), period_7 varchar(100), period_8 varchar(100), period_9 varchar(100));"
+    cursor.execute(query1)
+    connection.commit()
+    add_new_class(class_name)
+    for day in data['periods']:
+        query_insert = f"insert into {class_name} values('{day}', '{data['periods'][day][0]}', '{data['periods'][day][1]}', '{data['periods'][day][2]}', '{data['periods'][day][3]}', '{data['periods'][day][4]}', '{data['periods'][day][5]}', '{data['periods'][day][6]}', '{data['periods'][day][7]}', '{data['periods'][day][8]}');"
+        cursor.execute(query_insert)
+        connection.commit()
+            
+    return True     # Insertion Success
+    # except:
+    #     return False        #Insertion failed
+
+def add_new_class(class_name):
+    time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    class_name = class_name.split('_')[1] + '-' + class_name.split('_')[0] + '-' + class_name.split('_')[-1]
+    queryfinal = f'insert into classUpdated values("{class_name}", "{time}")'
+    print(queryfinal)
+    cursor.execute(queryfinal)
+    connection.commit()
+    
+def get_class_data(class_name):
+    # Check whether class data is present
+    query_to_check_presence_class = f'select * from classUpdated where class_updated in ("{class_name}");'
+    cursor.execute(query_to_check_presence_class)
+    result = cursor.fetchall()
+    if(len(result) == 0):
+        return {'400':'not found'}
+    else:
+        query_to_get_data = f'select * from {class_name};' 
+        query_to_fetch_sub_name = f"select * from {class_name.split('_')[0] + '_' + class_name.split('_')[1]};"
+        class_dict = {
+            'class_name': '',
+            'periods': {
+            },
+            'last_updated': result[-1][-1].strftime('%Y-%m-%d %H:%M:%S')
+        }
+        
+        class_dict['class_name'] = class_name.split('_')[1] + '-' + class_name.split('_')[0] + '-'+ class_name.split('_')[-1]
+        cursor.execute(query_to_get_data)
+        response = cursor.fetchall()
+        
+        cursor.execute(query_to_fetch_sub_name)
+        sub_code_to_name = dict()
+        for course_code, course_name in  cursor.fetchall():
+            sub_code_to_name.update({
+                course_code: course_name
+            })
+            
+        for i in response:
+            day_dict = {
+                
+            }
+            current_day = i[0]
+            
+            day_dict.update(
+                {
+                    current_day: []
+                }
+            )
+            for period in i[1:]:
+                if 'BREAK' in period or 'LUNCH' in period:
+                    break_name, break_time = period.split('/')
+                    day_dict[current_day].append([break_name,break_time])
+                else:
+                    sub_name, venue, time = period.split('/')
+                    if 'lab' not in sub_name.lower():
+                        try:
+                            sub_abbr = sub_code_to_name[sub_name]
+                        except:
+                            sub_abbr = 'Unknown'
+                    else:
+                        sub_abbr = 'LAB'
+                    day_dict[current_day].append([
+                        sub_name,
+                        sub_abbr,
+                        venue,
+                        time
+                    ])
+            class_dict['periods'].update(day_dict)
+    return class_dict
+
+# True - Data present alread
+# False - Data not present
+def check_class(class_name):
+    query = f'select class_updated from classUpdated;'
+    cursor.execute(query)
+    result = [i[0] for i in cursor.fetchall()]
+    if class_name in result:
+        return True
+    return False
+        
+try:
+    connection = db.connect(
+                host='db',
+                database='rec_companion',
+                passwd='root',
+                user='root'
+    )
+    cursor = connection.cursor()
+        # cursor.execute(q)
+        # connection.commit()
+        # print("Executed")
+        # cursor.sa
+    # cursor.close()
+    # connection.close()
+except:
+    print("Something went wrong with db")
--- server/main.py
+++ server/main.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, flash, jsonify
 from werkzeug.utils import secure_filename
 
 from fun.read_xlsx import read_xls
+from fun.db import *
 
 app = Flask(__name__)
 app.secret_key = "secret"
@@ -21,52 +22,95 @@ JOSN_PATH = os.path.join(os.getcwd(), 'timetable_upload/json')
 # make file upload - Done
 # read from excel file - Done
 
+@app.route('/uploadtimetable', methods=['GET', 'POST'])
+def upload_time_table():
+    if request.method == 'POST':
+        class_dict = {
+            'class_name': '',
+            'periods': {
+                
+            }
+        }
+        
+        class_dict['class_name'] = request.form.get('class_name')
+        for i in range(5):
+            current_day = request.form.get('day_' + str(i))
+            
+            periods = [request.form.get('period_' + str(i) + str(j)) for j in range(1,10)]
+            class_dict['periods'].update({
+                current_day: periods
+            })
+        
+        if upload_timetable(class_dict):
+            # on uploading done
+            flash("Upload Success")
+            return redirect("upload")
+        else:
+            return "<h1>Something went wrong while uploading</h1>"
+    else:
+        return render_template('table_upload.html')
+
+@app.route('/check_existence', methods=['GET'])
+def check_with_db():
+    class_name = request.args.get("class_name")
+    check = check_class(class_name)
+    if not check:
+        return redirect('/uploadtimetable')
+    else:
+        flash('Class data present!')
+        return redirect('upload')
+        
 
 @app.route('/upload', methods=['GET'])
 def index():
-    return render_template('upload.html')
+    return render_template('upload_1.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload_files():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect('upload')
+# @app.route('/upload', methods=['POST'])
+# def upload_files():
+#     pass
+#     if 'file' not in request.files:
+#         flash('No file part')
+#         return redirect('upload')
 
-    file = request.files['file']
-    # obtaining the name of the destination file
-    filename = file.filename
-    if filename == '':
-        flash('No file selected for uploading')
-        return redirect('upload')
-    else:
-        file_ext = os.path.splitext(filename)[1]  # Extracting extension
-        if file_ext in allowed_extensions:
-            secure_fname = secure_filename(filename)
-            file_path = os.path.join(UPLOAD_PATH, secure_fname)
-            file.save(file_path)
-            flash('File uploaded successfully')
-            read_xls(file_path, JOSN_PATH)
-            return redirect('/name')
-        else:
-            flash('Not allowed file type')
-            return redirect('upload')
+#     file = request.files['file']
+#     # obtaining the name of the destination file
+#     filename = file.filename
+#     if filename == '':
+#         flash('No file selected for uploading')
+#         return redirect('upload')
+#     else:
+#         file_ext = os.path.splitext(filename)[1]  # Extracting extension
+#         if file_ext in allowed_extensions:
+#             secure_fname = secure_filename(filename)
+#             file_path = os.path.join(UPLOAD_PATH, secure_fname)
+#             file.save(file_path)
+#             flash('File uploaded successfully')
+#             read_xls(file_path, JOSN_PATH)
+#             return redirect('/name')
+#         else:
+#             flash('Not allowed file type')
+#             return redirect('upload')
 
 
 def create_tt(class_name):
     # add a way to check if the response cnotains all the expected parameters
-    if len(class_name.split('-')) == 0:
-        return None
-    xls_path = os.path.join(JOSN_PATH, class_name.upper()+'.json')
-    try:
-        with open(xls_path, 'r') as f:
-            res = json.load(f)
-    except:
-        res = {'Not Found': 'Upload it'}
-    data = res
+    # if len(class_name.split('-')) == 0:
+    #     return None
+    # xls_path = os.path.join(JOSN_PATH, class_name.upper()+'.json')
+    # try:
+    #     with open(xls_path, 'r') as f:
+    #         res = json.load(f)
+    # except:
+    #     res = {'Not Found': 'Upload it'}
+    # data = res
+    
+    #class_name of form dept_year_sec
+    
+    res = get_class_data(class_name)
 
-    data = jsonify(data)
-    return data
+    res = jsonify(res)
+    return res
 
 
 @app.route("/<name>", methods=['GET', 'POST'])
